3c18f933092c9f11d06a025e13618b7cc0599cb7
Manuel Castañón-Puga <puga@uabc.edu.mx>
2019-04-11 15:35:44 -0700

    Ejemplo de clase del 11 de abril de 2019.

--- Agile Process/src/agile/process/CompanyGUI.java
+++ Agile Process/src/agile/process/CompanyGUI.java
@@ -6,26 +6,146 @@
 package agile.process;
 
 import Task_board.Board;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import javax.swing.JFrame;
+import sim.display.Console;
+import sim.display.Controller;
+import sim.display.Display2D;
 import sim.display.GUIState;
 import sim.engine.SimState;
+import sim.portrayal.DrawInfo2D;
+import sim.portrayal.Inspector;
+import sim.portrayal.continuous.ContinuousPortrayal2D;
+import sim.portrayal.network.NetworkPortrayal2D;
+import sim.portrayal.network.SimpleEdgePortrayal2D;
+import sim.portrayal.network.SpatialNetwork2D;
+import sim.portrayal.simple.CircledPortrayal2D;
+import sim.portrayal.simple.LabelledPortrayal2D;
+import sim.portrayal.simple.MovablePortrayal2D;
+import sim.portrayal.simple.OvalPortrayal2D;
 
 /**
  *
  * @author puga
  */
 public class CompanyGUI extends GUIState{
-    Board board = new Board("My board");
+
     
+public Display2D display;
+    public JFrame displayFrame;
+    ContinuousPortrayal2D yardPortrayal = new ContinuousPortrayal2D();
+    NetworkPortrayal2D buddiesPortrayal = new NetworkPortrayal2D();
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        CompanyGUI vid = new CompanyGUI();
+        Console c = new Console(vid);
+        c.setVisible(true);
+    }
+
+    public static Object getInfo() {
+        try {
+            return new java.net.URL("http://cyc.tij.uabc.mx/sim.html");
+        } catch (java.net.MalformedURLException e) {
+            return "Oops";
+        }
+    }
+
+    public CompanyGUI() {
+        super(new Project(System.currentTimeMillis()));
+    }
+
     public CompanyGUI(SimState state) {
         super(state);
     }
 
-    public Board getBoard() {
-        return board;
+    public static String getName() {
+        return "Employees Company-yard Cliques";
     }
 
-    public void setBoard(Board board) {
-        this.board = board;
+    @Override
+    public Object getSimulationInspectedObject() {
+        return state;
+    }
+
+    @Override
+    public Inspector getInspector() {
+        Inspector i = super.getInspector();
+        i.setVolatile(true);
+        return i;
+    }
+
+    @Override
+    public void start() {
+        super.start();
+        setupPortrayals();
+    }
+
+    @Override
+    public void load(SimState state) {
+        super.load(state);
+        setupPortrayals();
+    }
+
+    public void setupPortrayals() {
+        Project project = (Project) state;
+        // tell the portrayals what to portray and how to portray them
+        yardPortrayal.setField(project.yard);
+
+        yardPortrayal.setPortrayalForAll(
+                new MovablePortrayal2D(
+                        new CircledPortrayal2D(
+                                new LabelledPortrayal2D(
+                                        new OvalPortrayal2D() {
+                                    public void draw(Object object, Graphics2D graphics, DrawInfo2D info) {
+                                        Employee employee = (Employee) object;
+                                        int agitationShade = (int) (employee.getAgitation() * 255 / 10.0);
+                                        if (agitationShade > 255) {
+                                            agitationShade = 255;
+                                        }
+                                        paint = new Color(agitationShade, 0, 255 - agitationShade);
+                                        super.draw(object, graphics, info);
+                                    }
+                                },
+                                        5.0, null, Color.black, true),
+                                0, 5.0, Color.green, true)));
+
+        buddiesPortrayal.setField(new SpatialNetwork2D(project.yard, project.buddies));
+        buddiesPortrayal.setPortrayalForAll(new SimpleEdgePortrayal2D());
+
+        // reschedule the displayer
+        display.reset();
+        display.setBackdrop(Color.white);
+        // redraw the display display.repaint();
+    }
+
+    @Override
+    public void init(Controller c) {
+        super.init(c);
+        // make the displayer
+        display = new Display2D(600, 600, this);
+        // turn off clipping
+        display.setClipping(false);
+
+        displayFrame = display.createFrame();
+        displayFrame.setTitle("Company-yard Display");
+        c.registerFrame(displayFrame); // register the frame so it appears in the "Display" list displayFrame.setVisible(true);
+        display.attach(buddiesPortrayal, "Buddies");
+        display.attach(yardPortrayal, "Yard");
+    }
+
+    @Override
+    public void quit() {
+        super.quit();
+        if (displayFrame != null) {
+            displayFrame.dispose();
+        }
+        displayFrame = null;
+        display = null;
     }
     
 }
--- Agile Process/src/agile/process/Project.java
+++ Agile Process/src/agile/process/Project.java
@@ -5,16 +5,175 @@
  */
 package agile.process;
 
+import Task_board.Board;
+import Task_board.Issue;
+import Task_board.SwimLane;
 import sim.engine.SimState;
+import static sim.engine.SimState.doLoop;
+import sim.engine.Steppable;
+import sim.field.continuous.Continuous2D;
+import sim.field.network.Network;
+import sim.util.Bag;
+import sim.util.Double2D;
 
 /**
  *
  * @author puga
  */
 public class Project extends SimState{
+    Board board = new Board("My board");
     
-    public Project(long seed) {
+    public Continuous2D yard = new Continuous2D(1.0, 100, 100);
+
+    public double TEMPERING_CUT_DOWN = 0.99;
+    public double TEMPERING_INITIAL_RANDOM_MULTIPLIER = 10.0;
+    public boolean tempering = true;
+
+        public Project(long seed) {
         super(seed);
+        
+        System.out.println(board);
+        
+        SwimLane swimLaneBacklog = new SwimLane("Backlog", board);
+        board.swimLanes.add(swimLaneBacklog);
+        
+        System.out.println(board);
+        
+        SwimLane swimLaneToDo = new SwimLane("ToDo", board);
+        board.swimLanes.add(swimLaneToDo);
+        
+        System.out.println(board);
+        
+        SwimLane swimLaneDone = new SwimLane("Done", board);
+        board.swimLanes.add(swimLaneDone);
+        
+        System.out.println(board);
+        
+        Issue task1 = new Issue("T1","Tarea 1", "Descripcion", board, swimLaneBacklog);
+        swimLaneBacklog.getTasks().add(task1);
+        board.getTasks().add(task1);
+        
+        System.out.println(board);
+        
+    }
+    
+    
+
+    public boolean isTempering() {
+        return tempering;
+    }
+
+    public void setTempering(boolean val) {
+        tempering = val;
+    }
+
+    public int numEmployees = 10;
+    double forceToSchoolMultiplier = 0.01;
+    double randomMultiplier = 0.1;
+
+    public int getNumEmployees() {
+        return numEmployees;
+    }
+
+    public void setNumEmployees(int val) {
+        if (val > 0) {
+            numEmployees = val;
+        }
+    }
+
+    public double getForceToCompanyMultiplier() {
+        return forceToSchoolMultiplier;
+    }
+
+    public void setForceToCompanyMultiplier(double val) {
+        if (forceToSchoolMultiplier >= 0.0) {
+            forceToSchoolMultiplier = val;
+        }
+    }
+
+    public double getRandomMultiplier() {
+        return randomMultiplier;
+    }
+
+    public void setRandomMultiplier(double val) {
+        if (randomMultiplier >= 0.0) {
+            randomMultiplier = val;
+        }
+    }
+
+    public Object domRandomMultiplier() {
+        return new sim.util.Interval(0.0, 100.0);
+    }
+
+    public double[] getAgitationDistribution() {
+        Bag employees = buddies.getAllNodes();
+        double[] distro = new double[employees.numObjs];
+        for (int i = 0; i < employees.numObjs; i++) {
+            distro[i] = ((Employee) (employees.objs[i])).getAgitation();
+        }
+        return distro;
+    }
+
+    public Network buddies = new Network(false);
+
+
+
+    @Override
+    public void start() {
+        super.start();
+        
+        // add the tempering agent
+        if (tempering) {
+            randomMultiplier = TEMPERING_INITIAL_RANDOM_MULTIPLIER;
+            schedule.scheduleRepeating(schedule.EPOCH, 1, new Steppable() {
+                @Override
+                public void step(SimState state) {
+                    if (tempering) {
+                        randomMultiplier *= TEMPERING_CUT_DOWN;
+                    }
+                }
+            });
+        }
+        
+        // clear the yard
+        yard.clear();
+        // clear the buddies
+        //buddies.clear();
+        // add some employees to the yard
+        for (int i = 0; i < numEmployees; i++) {
+            Employee employee = new Employee(board);
+            yard.setObjectLocation(employee,
+                    new Double2D(yard.getWidth() * 0.5 + random.nextDouble() - 0.5,
+                            yard.getHeight() * 0.5 + random.nextDouble() - 0.5));
+
+            buddies.addNode(employee);
+            schedule.scheduleRepeating(employee);
+        }
+
+        // define like/dislike relationships
+        Bag employees = buddies.getAllNodes();
+        for (int i = 0; i < employees.size(); i++) {
+            Object employee = employees.get(i);
+            // who does he like?
+            Object employeeB = null;
+            do {
+                employeeB = employees.get(random.nextInt(employees.numObjs));
+            } while (employee == employeeB);
+            double buddiness = random.nextDouble();
+            buddies.addEdge(employee, employeeB, new Double(buddiness));
+            // who does he dislike?
+            do {
+                employeeB = employees.get(random.nextInt(employees.numObjs));
+            } while (employee == employeeB);
+            buddiness = random.nextDouble();
+            buddies.addEdge(employee, employeeB, new Double(-buddiness));
+        }
+
+    }
+
+    public static void main(String[] args) {
+        doLoop(Project.class, args);
+        System.exit(0);
     }
     
 }
